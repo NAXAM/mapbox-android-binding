680aa2633883f9d02a08bd20fbfd7de65e6fed7e
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-18 03:04:42 +0700

    A very simple demo app.

--- mapboxandroidnavigationsdk-droid/Naxam.MapboxNavigation.Droid/Additions/Classes.cs
+++ mapboxandroidnavigationsdk-droid/Naxam.MapboxNavigation.Droid/Additions/Classes.cs
@@ -1,4 +1,47 @@
-﻿namespace Com.Mapbox.Services.Android.Navigation.V5.Milestone
+﻿using ARExt = Android.Runtime.Extensions;
+namespace Com.Mapbox.Services.Android.Navigation.V5.Navigation
+{
+    using System.Threading.Tasks;
+    using Com.Mapbox.Api.Directions.V5.Models;
+    using Square.Retrofit2;
+    using Java.Lang;
+
+    public static class NavigationRouteExtensions
+    {
+        public static Task<DirectionsResponse> GetRouteAsync(this NavigationRoute route)
+        {
+            var tcs = new TaskCompletionSource<DirectionsResponse>();
+
+            route.GetRoute(new InternallCallback(tcs));
+
+            return tcs.Task;
+        }
+
+        class InternallCallback : Java.Lang.Object, ICallback
+        {
+            readonly TaskCompletionSource<DirectionsResponse> tcs;
+
+            public InternallCallback(TaskCompletionSource<DirectionsResponse> tcs)
+            {
+                this.tcs = tcs;
+            }
+
+            public void OnFailure(ICall p0, Throwable p1)
+            {
+                tcs.SetException(p1);
+            }
+
+            public void OnResponse(ICall p0, Response p1)
+            {
+                var result = ARExt.JavaCast<DirectionsResponse>(p1.Body());
+
+                tcs.TrySetResult(result);
+            }
+        }
+    }
+}
+
+namespace Com.Mapbox.Services.Android.Navigation.V5.Milestone
 {
     partial class RouteMilestone
     {
--- mapboxandroidnavigationsdk-droid/demo/MapboxNavigationQs/MainActivity.cs
+++ mapboxandroidnavigationsdk-droid/demo/MapboxNavigationQs/MainActivity.cs
@@ -1,13 +1,16 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Com.Mapbox.Services.Android.Navigation.V5.Navigation;
+using Com.Mapbox.Mapboxsdk;
+using Com.Mapbox.Geojson;
+using Android.Widget;
 
 namespace MapboxNavigationQs
 {
     [Activity(Label = "MapboxNavigationQs", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        const string MAPBOX_KEY = "pk.eyJ1IjoibmF4YW10ZXN0IiwiYSI6ImNqNWtpaG1oYTJqZmQyd28yM2tsdDhucmEifQ.Zr35ENq9tziBBDAbdeU2dw";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,12 +19,33 @@ namespace MapboxNavigationQs
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            Mapbox.GetInstance(this, MAPBOX_KEY);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            var btn = FindViewById<Button>(Resource.Id.myButton);
+            btn.Click += delegate {
+                BuildRoute();
+            };
+        }
+
+        async void BuildRoute()
+        {
+            MapboxNavigation navigation = new MapboxNavigation(this, MAPBOX_KEY);
+
+            Point origin = Point.FromLngLat(-77.03613, 38.90992);
+            Point destination = Point.FromLngLat(-77.0365, 38.8977);
+
+            var response = await NavigationRoute
+                .GetBuilder()
+                .AccessToken(Mapbox.AccessToken)
+                .Origin(origin)
+                .Destination(destination)
+                .Build()
+                .GetRouteAsync();
+
+            System.Diagnostics.Debug.WriteLine(response);
         }
     }
+
+
 }
 
