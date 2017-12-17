f4eee2bafda01c68ad215fb89384881cfb44a3b1
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-18 01:45:03 +0700

    SdkServices

--- mapboxsdkservice-droid/Naxam.MapboxSdkServices.Droid/Properties/AssemblyInfo.cs
+++ mapboxsdkservice-droid/Naxam.MapboxSdkServices.Droid/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MapboxSdkServiceQs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Naxam.MapboxSdkServices.Droid")]
+[assembly: AssemblyDescription("Xamarin.Android binding library - Mapbox SDK Services")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MapboxSdkServiceQs")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM CO.,LTD")]
+[assembly: AssemblyProduct("XBindings")]
+[assembly: AssemblyCopyright("Copyright © NAXAM 2017")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
@@ -26,5 +26,4 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
