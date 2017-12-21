3c318d4b06de7d0b112257e6d4567957f2a24265
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-21 10:48:18 +0700

    Mapbox Navigation UI 0.7.1

---
+++ mapboxnavigationui-droid/Naxam.MapboxNavigationUI.Droid/Additions/Classes.cs
@@ -0,0 +1,27 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using RecyclerViewAdapter = Android.Support.V7.Widget.RecyclerView.Adapter;
+using AndroidRuntime = Android.Runtime;
+using ARE = Android.Runtime.Extensions;
+using Android.OS;
+using Java.Lang;
+
+namespace Com.Mapbox.Services.Android.Navigation.UI.V5.Feedback
+{
+
+}
+
+namespace Com.Mapbox.Services.Android.Navigation.UI.V5.Voice.Polly
+{
+    partial class InstructionTask : AsyncTask
+    {
+        protected override unsafe Java.Lang.Object DoInBackground(params Object[] parameters)
+        {
+            var jarray = AndroidRuntime.JavaArray<Object>.FromArray<Object>(parameters);
+
+            var result = DoInBackground(AndroidRuntime.JNIEnv.GetArray<string>(jarray.Handle));
+
+            return new Java.Lang.String(result);
+        }
+    }
+}
